25ae961a848d55a7fbfc91f18f7b876992d993b2
Telnesito <carlosj_1703@hotmail.com>
2024-02-29 02:22:24 -0400

    Se agrego el URI de mongo a una variable de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { EmpresasModule } from './empresas/empresas.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: 'schema.gql',
-      driver: ApolloDriver
+      driver: ApolloDriver,
+
     }),
+    ConfigModule.forRoot({
+      envFilePath: '.develop.env'
+    }),
+
     EmpresasModule,
-    MongooseModule.forRoot('mongodb+srv://telne:1HpQ12ItyMAErNUp@companys.z05fhdl.mongodb.net/?retryWrites=true&w=majority&appName=Companys'),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI
+      })
+    }),
   ],
   controllers: [],
   providers: [],
