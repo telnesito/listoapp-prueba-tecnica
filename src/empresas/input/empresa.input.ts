d0c03e797e25410e788482e71cb87f18592914be
Telnesito <carlosj_1703@hotmail.com>
2024-02-29 01:40:20 -0400

    Se agrego descripcion al decorador @field que explica el atributo

--- src/empresas/input/empresa.input.ts
+++ src/empresas/input/empresa.input.ts
@@ -3,47 +3,47 @@ import { IsEmail, IsNumberString, } from "class-validator";
 @InputType()
 export class CompanyInput {
 
-  @Field()
+  @Field({ description: 'El nombre de la empresa' })
   readonly name!: string;
 
-  @Field()
+  @Field({ description: 'Una breve descripción de la empresa y sus actividades' })
   readonly description: string;
 
-  @Field()
+  @Field({ description: 'La ubicación física de la empresa (dirección, ciudad, país)' })
   readonly location: string;
 
-  @Field()
+  @Field({ description: ' El sector al que pertenece la empresa (tecnología, salud, finanzas, etc.)' })
   readonly sector: string;
 
-  @Field(() => Int)
+  @Field(() => Int, { description: ' La cantidad de empleados que tiene la empresa.' })
   readonly employees: number;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: 'Los ingresos anuales de la empresa' })
   readonly annualRevenue: number;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: 'La dirección web de la empresa' })
   readonly website: string;
   @IsEmail()
-  @Field()
+  @Field({ description: 'El correo electrónico de contacto de la empresa' })
   readonly email: string;
 
-  @Field()
+  @Field({ description: 'La fecha en que fue fundada la empresa.' })
   readonly foundedDate: Date;
 
   @IsNumberString()
-  @Field()
+  @Field({ description: 'El número de teléfono de la empresa.' })
   readonly phone: string;
 
-  @Field()
+  @Field({ description: 'Estado actual de la empresa (activo, inactivo, en proceso de liquidación, etc.)' })
   readonly status: string;
 
-  @Field()
+  @Field({ description: 'Puede ser una clasificación como PYME, startup, corporación, etc.' })
   readonly companyType: string;
-
-  @Field()
+  // Este campo almacena
+  @Field({ description: 'Este campo almacena el servicios que ofrezca la empresa' })
   readonly services: string;
 
-  @Field({ nullable: true })
+  @Field({ nullable: true, description: 'Enlaces a perfiles de redes sociales de la empresa (LinkedIn, Twitter, etc.).' })
   readonly socialMedia: string;
 
 }
